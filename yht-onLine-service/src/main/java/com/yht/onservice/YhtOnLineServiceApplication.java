9543b2ba82fa80a35eaf443e048d19bffba27459
”yht“ <17602657100@163.com>
2022-12-06 15:20:38 +0800

    增加结构

---
+++ yht-onLine-service/src/main/java/com/yht/onservice/YhtOnLineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yht.onservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhtOnLineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhtOnLineServiceApplication.class, args);
+    }
+
+}
---
+++ yht-onLine-service/src/test/java/com/yht/onservice/YhtOnLineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yht.onservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhtOnLineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
